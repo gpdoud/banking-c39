5b7ebee9750dc8ae98638bd5f479862092d38e61
Greg Doud <gpdoud@gmail.com>
2023-02-16 16:12:10 -0500

    Add project files.

---
+++ banking-c39/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using banking_c39;
+
+using System.Runtime.Intrinsics.Arm;
+
+var s1 = new SavingsComp();
+
+s1.AccountId = "Greg";
+s1.Description = "My Savings Account";
+s1.Balance = 1000;
+
+//Console.WriteLine($"S1 paid interest of {interest:C}");
+
+Console.WriteLine($"S1 balance is {s1.Balance:C}");
